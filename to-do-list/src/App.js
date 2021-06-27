1d9604ef4bb9d8cfffd23bd56f108b72a60acaab
Felipe Barbosa <felipeba@br.ibm.com>
2021-06-27 13:54:15 -0300

    fixes in list

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -5,16 +5,18 @@ import React, { useEffect, useState } from 'react';
 
 function App() {
 
-  const [itens, setItens] = useState([])
+  const [itens, setItens] = useState([
+    {"_id":1, "texto": "texto1"}, {"_id":1, "texto": "texto1"}, {"_id":1, "texto": "texto1"}
+  ])
 
-  useEffect(() => {
-    fetch('http://localhost:3000/to-do/list')
-      .then(response => response.json())
-      .then(data => {
-        console.log(data)
-        setItens(data.data)
-      });
-  }, [])
+  // useEffect(() => {
+  //   fetch('http://localhost:3000/to-do/list')
+  //     .then(response => response.json())
+  //     .then(data => {
+  //       console.log(data)
+  //       setItens(data.data)
+  //     });
+  // }, [])
 
   return (
     <div className="main">
@@ -23,8 +25,10 @@ function App() {
         {itens.map(item => {
           return <Item key={item._id} texto={item.texto} />
         })}
-        Visualizar: <button>Todos</button> | <button>Concluídos</button> | <button>Pendentes</button>
         <button>Inserir</button>
+        <div className="rowContainer">
+          <button>Todos</button> | <button>Concluídos</button> | <button>Pendentes</button>
+        </div>
       </div>
     </div>
   );
--- to-do-list/src/components/item.jsx
+++ to-do-list/src/components/item.jsx
@@ -6,7 +6,17 @@ export default class Item extends Component {
         return (
             <div className="card">
                 <input type="checkbox" key={this.props.key}/>
+                <div className="rowContainer">
+
+                <div className="row1">
                 {this.props.texto}
+                </div>
+                <div className="row2">
+                    <span>{"28/07"}</span>
+                     {/* <span>{"Pendente"}</span> */}
+                </div>
+                </div>
+                
                 <button>Apagar</button>
             </div>
         )
