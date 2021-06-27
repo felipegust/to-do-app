96e97be3244f0e13ba4be1bdce7735cbf33b3d55
Felipe Barbosa <felipeba@br.ibm.com>
2021-06-27 16:24:11 -0300

    added markComplete functionality

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,65 +1,90 @@
-import './App.css';
-import Item from '../src/components/item';
-import React, { useEffect, useState } from 'react';
+import "./App.css";
+import Item from "../src/components/item";
+import React, { useEffect, useState } from "react";
 // import fetch from 'fetch';
 
 function App() {
+	const [itens, setItens] = useState([
+		{ _id: 0, texto: "texto1", pendente: false },
+		{ _id: 1, texto: "texto1", pendente: true },
+		{ _id: 2, texto: "texto1", pendente: true },
+	]);
 
-  const [itens, setItens] = useState([
-    { "_id": 1, "texto": "texto1" }, { "_id": 2, "texto": "texto1" }, { "_id": 3, "texto": "texto1" }
-  ])
+	const [textoEdit, setTextoEdit] = useState("");
 
-  const [textoEdit, setTextoEdit] = useState("")
+	function handleAdd(e) {
+		console.log(e);
+		let id = itens[itens.length - 1]._id + 1;
+		setItens([...itens, { _id: id, texto: "", pendente: true}]);
+	}
 
-  function handleAdd(e) {
-    console.log(e)
-    let id = itens[itens.length - 1]._id + 1
-    setItens([...itens, { "_id": id, "texto": "" }])
+	function handleEdit(value) {
+		setTextoEdit(value);
+	}
+
+	function handleKeyPress(event) {
+		if (event.key === "Enter") {
+			let newItens = [...itens];
+			newItens[newItens.length - 1].texto = textoEdit;
+			setItens([...newItens]);
+			setTextoEdit("");
+		}
+	}
+
+	function handleDelete(_id, event) {
+		let newItens = [...itens];
+		newItens.splice(_id, 1);
+		setItens([...newItens]);
+	}
+
+  function handleUpdate(_id, event) {
+    let newItens = [...itens]
+    newItens[_id].texto = textoEdit
+    setItens([...newItens]);
   }
 
-  function handleEdit(value) {
-    setTextoEdit(value)
+  function handleStatus(_id, event) {
+    let newItens = [...itens]
+    newItens[_id].pendente = !newItens[_id].pendente
+    setItens([...newItens]);
   }
 
-  function handleKeyPress(event) {
-    if (event.key === 'Enter') {
-      let newItens = [...itens]
-      newItens[newItens.length - 1].texto = textoEdit
-      setItens([...newItens])
-      setTextoEdit("")
-    }
-  }
+	// useEffect(() => {
+	//   fetch('http://localhost:3000/to-do/list')
+	//     .then(response => response.json())
+	//     .then(data => {
+	//       console.log(data)
+	//       setItens(data.data)
+	//     });
+	// }, [])
 
-  function handleDelete(event) {
-    console.log(event)
-  }
-
-  // useEffect(() => {
-  //   fetch('http://localhost:3000/to-do/list')
-  //     .then(response => response.json())
-  //     .then(data => {
-  //       console.log(data)
-  //       setItens(data.data)
-  //     });
-  // }, [])
-
-  return (
-    <div className="main">
-      <div className="to-do-list">
-        <h1>To Do List</h1>
-        {itens.map(item => {
-          return <Item key={item._id} texto={item.texto} onDelete={handleDelete} onChange={handleEdit} onKeyPress={handleKeyPress} />
-        })}
-        <div className="rowContainer">
-
-          <button onClick={(e) => handleAdd(e)}>Inserir</button>
-        </div>
-        <div className="rowContainer">
-          <span>Concluídos</span><span>Pendentes</span>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="main">
+			<div className="to-do-list">
+				<h1>To Do List</h1>
+				{itens.map((item) => {
+					return (
+						<Item
+							key={item._id}
+							texto={item.texto}
+              pendente={item.pendente}
+              onStatus={(e) => handleStatus(item._id, e)}
+							onDelete={(e) => handleDelete(item._id, e)}
+							onChange={handleEdit}
+							onKeyPress={handleKeyPress}
+						/>
+					);
+				})}
+				<div className="rowContainer">
+					<button onClick={(e) => handleAdd(e)}>Inserir</button>
+				</div>
+				<div className="rowContainer">
+					<span>Concluídos</span>
+					<span>Pendentes</span>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- to-do-list/src/components/item.jsx
+++ to-do-list/src/components/item.jsx
@@ -1,40 +1,63 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 function Item(props) {
+	function handleChange(e) {
+		props.onChange(e.target.value);
+	}
 
-    function handleChange(e) {
-        props.onChange(e.target.value);
-    }
+	function handleKeyPress(e) {
+		props.onKeyPress(e);
+	}
 
-    function handleKeyPress(e) {
-        props.onKeyPress(e)
-    }
+	function handleDelete(e) {
+		props.onDelete(e);
+	}
 
-    function handleDelete(e) {
-        props.onDelete(e)
-    }
+	function handleStatus(e) {
+		props.onStatus(e);
+	}
 
-    return (
-        <div className="card">
-            <input type="checkbox" key={props.key} />
-            <div className="rowContainer">
-
-                <div className="row1">
-                    {props.texto ? props.texto : <input type="text" value={props.value} onChange={handleChange} onKeyPress={handleKeyPress} />}
-                </div>
-                {/* <div className="row2">
+	return (
+		<div className="card">
+			<input
+				type="checkbox"
+				key={props.key}
+				onClick={handleStatus}
+				checked={!props.pendente}
+			/>
+			<div className="rowContainer">
+				<div className="row1">
+					{props.texto ? (
+						<span
+							style={
+								props.pendente
+									? {}
+									: { textDecoration: "line-through" }
+							}
+						>
+							{props.texto}
+						</span>
+					) : (
+						<input
+							type="text"
+							value={props.value}
+							onChange={handleChange}
+							onKeyPress={handleKeyPress}
+						/>
+					)}
+				</div>
+				{/* <div className="row2">
                     <span>{"28/07"}</span>
                      <span>{"Pendente"}</span>
                 </div> */}
-            </div>
+			</div>
 
-            <button onClick={handleDelete}>Apagar</button>
-        </div>
-    )
-
+			<button onClick={handleDelete}>Apagar</button>
+		</div>
+	);
 }
 
-export default Item
+export default Item;
 // export default class Item extends Component {
 
 //     handleChange = (e) => {
